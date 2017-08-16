2e38bee88956fb9f93d7fd9ed32d389d7f492d67
Josh Weaver <jweaver911@gmail.com>
2017-08-15 19:25:21 -0500

    feat: add css transition group to books

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Book extends Component {
   handleChange = (e) => {
@@ -9,26 +10,33 @@ class Book extends Component {
     const BOOK = this.props.book;
     return (
       <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${BOOK.imageLinks && BOOK.imageLinks.thumbnail}")` }}></div>
-            <div className="book-shelf-changer">
-              <select value={BOOK.shelf || ''} onChange={this.handleChange}>
-                <option value="" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={600}
+          transitionEnter={false}
+          transitionLeave={false}>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${BOOK.imageLinks && BOOK.imageLinks.thumbnail}")` }}></div>
+              <div className="book-shelf-changer">
+                <select value={BOOK.shelf || ''} onChange={this.handleChange}>
+                  <option value="" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{BOOK.title || 'No title found'}</div>
+            <div>
+              {BOOK.authors && BOOK.authors.map((author, i) => (
+                <div key={i} className="book-authors">{author}</div>
+              ))}
             </div>
           </div>
-          <div className="book-title">{BOOK.title || 'No title found'}</div>
-          <div>
-            {BOOK.authors && BOOK.authors.map((author, i) => (
-              <div key={i} className="book-authors">{author}</div>
-            ))}
-          </div>
-        </div>
+        </ReactCSSTransitionGroup>
       </li>
     )
   }
